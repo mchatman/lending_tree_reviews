8357db4f6eab49672507ae269656df4bf825f790
Myles Chatman <myleschatman@gmail.com>
2022-11-27 22:50:43 -0800

    create class base views

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+from pydantic import HttpUrl
 
 from app import schemas
+from app.services.reviews import ReviewsService
 
-router = APIRouter()
+router = InferringRouter()
 
 
-@router.get("/reviews/{business_url}", response_model=schemas.Reviews)
-async def list_reviews(business_url: str):
-    return {"business_url": business_url}
+@cbv(router)
+class ReviewsListRouter:
+    reviews_service = ReviewsService()
+
+    @router.get("/reviews/{business_url:path}", response_model=schemas.Reviews)
+    async def read_reviews(self, business_url: HttpUrl):
+        """
+        Retrieve all reviews.
+        """
+        reviews = await self.reviews_service.find_reviews(business_url)
+        return reviews
