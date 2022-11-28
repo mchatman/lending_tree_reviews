5b0006e6f8a208ce0c259c567ab3cbb9af6a0ba6
Myles Chatman <myleschatman@gmail.com>
2022-11-27 21:39:38 -0800

    move to app dir

--- app/api/routers/reviews.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/reviews/{business_url}")
-async def list_reviews(business_url: str):
-    return {"business_url": business_url}
